0cf4b2107a47063b60edecafd2bd1a321cf7b42e
johannnsilva <johannn9896@gmail.com>
2025-07-24 21:47:34 +0530

    update some simple js

---
+++ day05/app.js
@@ -0,0 +1,15 @@
+
+let name = "sudarshana";
+
+let age = 20;
+
+let isadd = 20;
+
+let price = 100;
+
+
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof isadd); // number
+console.log(typeof price); // number
